04e89422016bdcf81ae84e4beabaae83537346f3
邹婉玉 <zouwanyu@sheyuan.com>
2020-03-09 01:03:06 +0800

    练习事件传递方法

---
+++ my-project/src/App.vue
@@ -0,0 +1,37 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <HelloWorld :msg="message"/> -->
+    <FatherComp />
+    <textarea>==============================</textarea>
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import FatherComp from './components/FatherComp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    // HelloWorld,
+    FatherComp
+  },
+  data() {
+    return {
+      message: "456"
+    }
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ my-project/src/components/FatherComp.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+		<textarea>父组件的toCity</textarea>
+        <textarea v-model="toCity"></textarea>
+        <train-city @showCityName="updateCity" :sendData="toCity"></train-city>
+    </div>
+</template>
+	
+<script>
+  import TrainCity from './train-city.vue'
+  export default {
+    name:'index',
+    components: {TrainCity},
+    data () {
+      return {
+        toCity:"北京"
+      }
+    },
+    methods:{
+      updateCity(data){//触发子组件城市选择-选择城市的事件
+        this.toCity = data.cityname;//改变了父组件的值
+        console.log('toCity:'+this.toCity)
+      }
+    }
+  }
+</script>

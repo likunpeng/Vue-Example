3fdeed8ade5d25fc225788dfcf33964e265b9c43
邹婉玉 <zouwanyu@sheyuan.com>
2020-03-09 01:39:17 +0800

    复习计算属性和侦听器 了解响应式依赖

--- my-project/src/components/FatherComp.vue
+++ my-project/src/components/FatherComp.vue
@@ -3,6 +3,14 @@
 		<textarea class="lkp-title">父组件的toCity</textarea>
         <textarea v-model="toCity"></textarea>
         <train-city @showCityName="updateCity" :sendData="toCity"></train-city>
+		<div id="example">
+			{{message.split('').reverse().join('')}}
+			
+				现在时间是：{{now}}
+				更新时间：{{updateNow()}}
+			
+		</div>
+		
     </div>
 </template>
 	
@@ -13,15 +21,24 @@
     components: {TrainCity},
     data () {
       return {
-        toCity:"北京"
+        toCity:"北京",
+		message: 'lkp'
       }
     },
     methods:{
       updateCity(data){//触发子组件城市选择-选择城市的事件
         this.toCity = data.cityname;//改变了父组件的值
         console.log('toCity:'+this.toCity)
-      }
-    }
+      },
+      updateNow(){
+		return Date.now();
+     }
+    },
+	computed: {
+		now: function(){
+			return Date.now();
+		}
+	}
   }
 </script>
 <style >

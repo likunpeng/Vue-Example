540384d81a9fae7c981abf757137de732178e82f
邹婉玉 <zouwanyu@sheyuan.com>
2020-03-11 18:29:00 +0800

    练习事件传递问题

--- my-project/src/App.vue
+++ my-project/src/App.vue
@@ -1,23 +1,28 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
+    <!-- <img alt="Vue logo" src="./assets/logo.png"> -->
     <!-- <HelloWorld :msg="message"/> -->
-    <FatherComp />
-    <textarea>==============================</textarea>
+    <!-- <FatherComp /> -->
+    <!-- <textarea>==============================</textarea> -->
 	<!-- `greet` 是在下面定义的方法名 -->
-	  <button v-on:click="greet">Greet</button>
+	<button v-on:click="greet">Greet</button>
+	<button v-on:click="say('hi')">Say hi</button>
+	<button v-on:click="say('what')">Say what</button>
+	<EventTest></EventTest>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import FatherComp from './components/FatherComp.vue'
+import EventTest from './components/EventTest.vue'
 
 export default {
   name: 'App',
   components: {
     // HelloWorld,
-    FatherComp
+    FatherComp,
+	EventTest
   },
   data() {
     return {
@@ -32,7 +37,10 @@ export default {
         if (event) {
           alert(event.target.tagName)
         }
-      }
+      },
+	  say: function (msg) {
+		  alert(msg)
+	  }
     }
 }
 </script>
---
+++ my-project/src/components/EventTest.vue
@@ -0,0 +1,25 @@
+<template>
+	<div>
+		<button v-on:click="warn('Form cannot be submitted yet.', $event)">
+		  Submit
+		</button>
+	</div>
+</template>
+
+<script>
+	export default {
+		methods: {
+			warn: function (message, event) {
+			    // 现在我们可以访问原生事件对象
+			    if (event) {
+			      event.preventDefault()
+			    }
+			    alert(message)
+			  }
+		}
+	}
+</script>
+
+<style>
+	
+</style>
